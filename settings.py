83d9340b9fc300217a7fa0f3ce84d7904a648992
Lucas Martins <dev.lucasmartins@gmail.com>
2019-02-07 13:57:47 -0200

    heroku deploy

--- settings.py
+++ settings.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv(join(dirname(__file__), '.env'))
 
+print(os.environ.get("PORT"))
+
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DATABASE_URI = os.environ.get("DATABASE_URI")
 PORT = os.environ.get("PORT")
