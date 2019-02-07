ce227a5ca273bddb245d0d38f3ae5abfd01c51ac
Lucas Martins <dev.lucasmartins@gmail.com>
2019-02-07 16:40:09 -0200

    a

--- sockets/sockets.py
+++ sockets/sockets.py
@@ -54,16 +54,14 @@ def on_message_created(data):
     user_id = str(request.args.get('user_id'))
 
     # datetime.fromtimestamp(data['createdAt'] / 1e3)
-    message_created_at = datetime.strptime(data.get('createdAt'), '%Y-%m-%dT%H:%M:%S.%fZ')
-    message_type = data['type'] if 'type' in data and data['type'] is not None else 0
+    created_at = datetime.strptime(data['createdAt'], '%Y-%m-%dT%H:%M:%S.%fZ')
 
-    # Contact.query.filter(Contact.fk_users_id == user_id).first(),
-    message = Message(content=data.get('content'),
-                      fk_contacts_id=data.get('contact').get('id'),
-                      fk_chats_id=data.get('chat').get('id'),
-                      type=message_type,
+    message = Message(content=data['content'],
+                      contact=Contact.query.filter(Contact.fk_users_id == user_id).first(),
+                      chat=data['chat'],
+                      type=data['type'] if 'type' in data and data['type'] is not None else 0,
                       status=1,
                       updated_at=datetime.now(),
-                      created_at=message_created_at)
+                      created_at=created_at)
 
     message_service.create_message(message)
