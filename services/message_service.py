4744c761255fde407cd329426ba359f3a00d68bb
Lucas Martins <dev.lucasmartins@gmail.com>
2019-03-21 15:16:29 -0300

    convert endpoint from string to dict

--- services/message_service.py
+++ services/message_service.py
@@ -1,5 +1,5 @@
 from database import db
-import database.models
+import database.models as models
 
 from sockets import sockets
 
@@ -16,16 +16,16 @@ def create_message(message):
     db.session.commit()
     
     # gets the reference by id from database.
-    message = Message.query.filter(Message.server_id == message.server_id).first()
+    message = models.Message.query.filter(models.Message.server_id == message.server_id).first()
     
     # Search for contacts related to the conversation.
-    chat_contacts = ChatContacts.query.filter(ChatContacts.fk_chats_id == message.fk_chats_id)
+    chat_contacts = models.ChatContacts.query.filter(models.ChatContacts.fk_chats_id == message.fk_chats_id)
 
     # Iterates the conversation contacts and inserts a row in message_contacts.
     for chat_contact in chat_contacts:
         if chat_contact.contact.fk_users_id is not None:
             user_id = str(round(chat_contact.contact.fk_users_id))
-            message_contact = MessageContact(message.server_id, chat_contact.contact.id)
+            message_contact = models.MessageContact(message.server_id, chat_contact.contact.id)
 
             db.session.add(message_contact)
             db.session.commit()
@@ -36,7 +36,7 @@ def create_message(message):
                     sockets.sockets[user_id].emit('message::created', message.as_json())
 
                     # Web Push Notifications
-                    user_endpoints = UserEndpoint.query.filter(UserEndpoint.fk_users_id == user_id)
+                    user_endpoints = models.UserEndpoint.query.filter(models.UserEndpoint.fk_users_id == user_id)
                     print('Iteating over user endpoints...')
                     for user_endpoint in user_endpoints:
                         data = notifications.WebPushNotificationData()
@@ -55,12 +55,12 @@ def create_message(message):
 
 def update_message_set_received(message_id, contact_id) -> None:
     """Sets a Message as Received by Message Id and Contact Id."""
-    message = Message.query.filter(Message.server_id == message_id).first()
-    contact = Contact.query.filter(Contact.id == contact_id).first()
+    message = models.Message.query.filter(models.Message.server_id == message_id).first()
+    contact = models.Contact.query.filter(models.Contact.id == contact_id).first()
 
-    db.session.query(MessageContact) \
-        .filter(MessageContact.fk_messages_id == message_id,
-                MessageContact.fk_contacts_id == contact_id) \
+    db.session.query(models.MessageContact) \
+        .filter(models.MessageContact.fk_messages_id == message_id,
+                models.MessageContact.fk_contacts_id == contact_id) \
         .update({'is_received': True})
     db.session.commit()
 
@@ -73,8 +73,8 @@ def update_message_set_received(message_id, contact_id) -> None:
             user_id = str(round(message.contact.user.id))
             if message.status < 2:
                 message.status = 2
-                db.session.query(Message) \
-                    .filter(Message.server_id == message_id) \
+                db.session.query(models.Message) \
+                    .filter(models.Message.server_id == message_id) \
                     .update({'status': message.status})
                 db.session.commit()
 
@@ -89,12 +89,12 @@ def update_message_set_received(message_id, contact_id) -> None:
 
 def update_message_set_seen(message_id, contact_id) -> None:
     """Sets a Message as Read by Message Id and Contact Id."""
-    message = Message.query.filter(Message.server_id == message_id).first()
-    contact = Contact.query.filter(Contact.id == contact_id).first()
+    message = models.Message.query.filter(models.Message.server_id == message_id).first()
+    contact = models.Contact.query.filter(models.Contact.id == contact_id).first()
 
-    db.session.query(MessageContact) \
-        .filter(MessageContact.fk_messages_id == message_id,
-                MessageContact.fk_contacts_id == contact_id) \
+    db.session.query(models.MessageContact) \
+        .filter(models.MessageContact.fk_messages_id == message_id,
+                models.MessageContact.fk_contacts_id == contact_id) \
         .update({'is_received': True, 'is_seen': True})
     db.session.commit()
 
@@ -107,8 +107,8 @@ def update_message_set_seen(message_id, contact_id) -> None:
             user_id = str(round(message.contact.user.id))
             if message.status < 3:
                 message.status = 3
-                db.session.query(Message) \
-                    .filter(Message.server_id == message_id) \
+                db.session.query(models.Message) \
+                    .filter(models.Message.server_id == message_id) \
                     .update({'status': message.status})
                 db.session.commit()
 
@@ -123,40 +123,40 @@ def update_message_set_seen(message_id, contact_id) -> None:
 
 def set_received_before_id(message_id, contact_id) -> None:
     """Sets a Message as Read by Message Id and Contact Id."""
-    message = Message.query.filter(Message.server_id == message_id).first()
-    contact = Contact.query.filter(Contact.id == contact_id).first()
+    message = models.Message.query.filter(models.Message.server_id == message_id).first()
+    contact = models.Contact.query.filter(models.Contact.id == contact_id).first()
 
-    db.session.query(MessageContact) \
-        .filter(MessageContact.fk_messages_id < message_id,
-                MessageContact.fk_contacts_id == contact_id) \
+    db.session.query(models.MessageContact) \
+        .filter(models.MessageContact.fk_messages_id < message_id,
+                models.MessageContact.fk_contacts_id == contact_id) \
         .update({'is_received': True})
     db.session.commit()
 
     if contact.id != message.contact.id:
         message.status = 2
-        db.session.query(Message) \
-            .filter(Message.server_id < message_id) \
-            .filter(Message.status < message.status) \
+        db.session.query(models.Message) \
+            .filter(models.Message.server_id < message_id) \
+            .filter(models.Message.status < message.status) \
             .update({'status': message.status})
         db.session.commit()
 
 
 def set_seen_before_id(message_id, contact_id) -> None:
     """Sets a Message as Received by Message Id and Contact Id."""
-    message = Message.query.filter(Message.server_id == message_id).first()
-    contact = Contact.query.filter(Contact.id == contact_id).first()
+    message = models.Message.query.filter(models.Message.server_id == message_id).first()
+    contact = models.Contact.query.filter(models.Contact.id == contact_id).first()
 
-    db.session.query(MessageContact) \
-        .filter(MessageContact.fk_messages_id < message_id,
-                MessageContact.fk_contacts_id == contact_id) \
+    db.session.query(models.MessageContact) \
+        .filter(models.MessageContact.fk_messages_id < message_id,
+                models.MessageContact.fk_contacts_id == contact_id) \
         .update({'is_received': True, 'is_seen': True})
     db.session.commit()
 
     if contact.id != message.contact.id:
         message.status = 3
-        db.session.query(Message) \
-            .filter(Message.server_id < message_id) \
-            .filter(Message.status < message.status) \
+        db.session.query(models.Message) \
+            .filter(models.Message.server_id < message_id) \
+            .filter(models.Message.status < message.status) \
             .update({'status': message.status})
         db.session.commit()
 
